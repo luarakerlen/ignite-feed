a99fb07fc75a7adae68f28f98bee916e8b776aa6
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-17 22:34:53 -0300

    chore: insert sidebar and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
-import './global.css'
+import styles from './App.module.css';
+import './global.css';
 
 export function App() {
 	return (
 		<div>
-			<Header/>
-			<Post
-				author='Luara Kerlen'
-				content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae non delectus officia, exercitationem doloribus aliquam nulla quod cum deserunt consequuntur quis voluptates fugiat itaque nostrum in magnam neque ad ut.'
-			/>
-			<Post author='Laissy' content='Outro post' />
+			<Header />
+			<div className={styles.wrapper}>
+				<aside>sidebar</aside>
+				<main>
+					<Post
+						author='Luara Kerlen'
+						content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae non delectus officia, exercitationem doloribus aliquam nulla quod cum deserunt consequuntur quis voluptates fugiat itaque nostrum in magnam neque ad ut.'
+					/>
+					<Post author='Laissy' content='Outro post' />
+				</main>
+			</div>
 		</div>
 	);
 }
