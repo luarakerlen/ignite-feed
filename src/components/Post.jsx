9f1b67d60717dbe339c88e904de90d4aa91d56d4
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-18 00:50:26 -0300

    feat: create comment area

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,14 @@ export function Post() {
 					<a href='#'> #rocketseat</a>
 				</p>
 			</div>
+
+			<form className={styles.commentForm}>
+				<strong>Deixe seu feedback</strong>
+				<textarea placeholder='Escreva um comentário...' />
+				<footer>
+					<button type='submit'>Publicar</button>
+				</footer>
+			</form>
 		</article>
 	);
 }
