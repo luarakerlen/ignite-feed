4ec2be451f284cc7ef75f4f6ae4970a2bdc5e8ec
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-18 16:12:22 -0300

    feat: create comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+	return (
+		<div className={styles.comment}>
+			<img
+				src='https://avatars.githubusercontent.com/u/26902816?v=4'
+				alt='Foto do usuário'
+			/>
+
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<strong>Luara Kerlen</strong>
+							<time title='11 de Maio às 08:13' dateTime='2022-05-11 08:13:30'>
+								Cerca de 1h atrás
+							</time>
+						</div>
+						<button title='Deletar comentário'>
+							<Trash size={20} />
+						</button>
+					</header>
+					<p>Muito bom Devon, parabéns!! 👏👏</p>
+				</div>
+				<footer>
+					<button>
+						<ThumbsUp /> Aplaudir <span>20</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	);
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './Header';
 export * from './Sidebar';
 export * from './Post';
+export * from './Comment';
