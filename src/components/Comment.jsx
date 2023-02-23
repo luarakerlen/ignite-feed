5b4607fb4670149984c71694e83cd9a0e359c21c
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-23 18:30:50 -0300

    feat: delete comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+	function handleDeleteComment() {
+		onDeleteComment(content);
+	}
+
 	return (
 		<div className={styles.comment}>
 			<Avatar
@@ -19,7 +23,7 @@ export function Comment({ content }) {
 								Cerca de 1h atrás
 							</time>
 						</div>
-						<button title='Deletar comentário'>
+						<button onClick={handleDeleteComment} title='Deletar comentário'>
 							<Trash size={24} />
 						</button>
 					</header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,14 @@ export function Post({ author, publishedAt, content }) {
 		setNewCommentText(event.target.value);
 	}
 
+	function deleteComment(commentToBeDeleted) {
+		const commentsWithoutDeletedOne = comments.filter((comment) => {
+			return comment !== commentToBeDeleted;
+		});
+
+		setComments(commentsWithoutDeletedOne);
+	}
+
 	return (
 		<article className={styles.post}>
 			<header>
@@ -80,7 +88,13 @@ export function Post({ author, publishedAt, content }) {
 
 			<div className={styles.commentList}>
 				{comments.map((comment) => {
-					return <Comment key={comment} content={comment} />;
+					return (
+						<Comment
+							key={comment}
+							content={comment}
+							onDeleteComment={deleteComment}
+						/>
+					);
 				})}
 			</div>
 		</article>
