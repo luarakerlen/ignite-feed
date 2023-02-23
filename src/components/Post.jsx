e6688ff1de5e7999f312fc88aedf9d749be5c19a
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-23 13:28:55 -0300

    chore: add keys

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -54,10 +54,10 @@ export function Post({ author, publishedAt, content }) {
 			<div className={styles.content}>
 				{content.map((line) => {
 					if (line.type === 'paragraph') {
-						return <p>{line.content}</p>;
+						return <p key={line.content}>{line.content}</p>;
 					} else if (line.type === 'link') {
 						return (
-							<p>
+							<p key={line.content}>
 								<a href='#'>{line.content}</a>
 							</p>
 						);
@@ -80,7 +80,7 @@ export function Post({ author, publishedAt, content }) {
 
 			<div className={styles.commentList}>
 				{comments.map((comment) => {
-					return <Comment content={comment} />;
+					return <Comment key={comment} content={comment} />;
 				})}
 			</div>
 		</article>
