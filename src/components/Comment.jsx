7ea50ae23e6e08fba44443e9adfcc7098a221455
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-23 19:31:43 -0300

    feat: like comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+	const [likeCount, setLikeCount] = useState(0);
+
 	function handleDeleteComment() {
 		onDeleteComment(content);
+	}
+
+	function handleLikeComment() {
+		setLikeCount(likeCount + 1);
 	}
 
 	return (
@@ -30,8 +37,8 @@ export function Comment({ content, onDeleteComment }) {
 					<p>{content}</p>
 				</div>
 				<footer>
-					<button>
-						<ThumbsUp /> Aplaudir <span>20</span>
+					<button onClick={handleLikeComment}>
+						<ThumbsUp /> Aplaudir <span>{likeCount}</span>
 					</button>
 				</footer>
 			</div>
